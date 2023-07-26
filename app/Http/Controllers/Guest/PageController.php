d4830ed17e43e083161ef6d9f89759b5eb280e1b
IanzanoLorenzo <ianzano.lorenzo@gmail.com>
2023-07-26 10:28:54 +0200

    first commit

---
+++ app/Http/Controllers/Guest/PageController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Guest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\Comics;
+
+class PageController extends Controller
+{
+    public function index(){
+        $comics = Comics::all();
+        dd($comics);
+        return view('welcome');
+    }
+}
